7faeddaf8faca483a72f4d3e3e02d017ea336148
Bastián Escribano <bastian.escribano@usach.cl>
2021-06-13 22:43:15 -0400

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -7,5 +7,5 @@ url = "{insertURLhere}"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
-eq = soup.find_all('{insertTAGhere', class_='insertCLASShere')
+eq = soup.find_all('{insertTAGhere}', class_='{insertCLASShere}')
 print(eq)
